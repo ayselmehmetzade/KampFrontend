4282637a62fa7114717352ba94f2299cb71d3cb7
ayselmehmetzade <ayselmehmetzade@hotmail.com>
2021-03-13 23:53:15 +0300

    13 mart kamp sonu

--- src/app/models/category-response-model.ts
+++
@@ -1,7 +0,0 @@
-import { Category } from "./category";
-import { ResponseModel } from "./response-model";
-
-
-export interface CategoryResponseModel extends ResponseModel{
-    data:Category[]
-}
--- src/app/models/product-response-model.ts
+++
@@ -1,7 +0,0 @@
-import { Product } from "./product";
-import { ResponseModel } from "./response-model";
-
-
-export interface ProductResponseModel extends ResponseModel{
-    data:Product[]
-}
