05ff7726397adc6c3131ee30a79dfc650a5ab1ab
Aysel Mammedzade <ayselmehmetzade@hotmail.com>
2021-03-21 00:02:10 +0300

    20 Mart (#2)
    * 17 mart kamp sonu
    
    * 20 mart FormsGroup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
@@ -16,6 +18,9 @@ import { FilterPipePipe } from './pipes/filter-pipe.pipe';
 import { ToastrModule } from 'ngx-toastr';
 import { CartSummaryComponent } from './components/cart-summary/cart-summary.component';
 
+import { ProductAddComponent } from './components/product-add/product-add.component';
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -25,12 +30,18 @@ import { CartSummaryComponent } from './components/cart-summary/cart-summary.com
     VatAddedPipe,
     FilterPipePipe,
     CartSummaryComponent,
+
+    ProductAddComponent
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
+
+    ReactiveFormsModule,
+
     BrowserAnimationsModule,
     ToastrModule.forRoot({
       positionClass:"toast-bottom-right"
